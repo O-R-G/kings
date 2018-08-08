7d5e2babea06d0b446f786f46c97ef4201a49a59
Eric Li <ericyoungli@gmail.com>
2018-08-08 15:18:33 -0400

    update kings

--- kings.sh
+++ kings.sh
@@ -1,21 +1,20 @@
-q#!/bin/bash
+#!/bin/bash
 
 # google cloud speech to text implemented with processing front end
 # (google cloud services account free 365-day trial)
 # https://cloud.google.com/speech-to-text/docs/
-# gc project kings-speech-to-text, gc bucket kings-speech-to-text 
+# gc project kings-speech-to-text, gc bucket kings-speech-to-text
 # authentication key as bash environment variable
 
+#KEY=/Users/eric/dev/kings-auth/kings-212617-06ed0601c807.json
+KEY=/Users/reinfurt/Documents/Projects/KINGS/software/google-cloud-platform/json/auth/kings-speech-to--1532733222205-b656714f6407.json
+#BUCKET=gs://kings-speech-to-text-2
+BUCKET=gs://kings-speech-to-text
 
-KEY="/Users/reinfurt/Documents/Projects/KINGS/software/google\ cloud\ platform/json/auth/kings-speech-to-text-cb6be9604529.json"
-# KEY="/Users/eric/dev/kings-auth/kings-212617-06ed0601c807.json"
 IN=data/speech.wav
 OUT=data/speech.wav
 TMP=data/speech-16k.wav
-JSON=data/txt.json	
-BUCKET=gs://kings-speech-to-text
-# BUCKET=gs://kings-speech-to-text-2
-
+JSON=data/txt.json
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -43,6 +42,7 @@ done
 
 echo "authenticate ..."
 
+gcloud auth activate-service-account --key-file=$KEY
 export GOOGLE_APPLICATION_CREDENTIALS=$KEY
 
 #
@@ -65,7 +65,7 @@ ffmpeg -i $IN -acodec pcm_s16le -ac 1 -ar 44100 $OUT
 # ffmpeg -i $OUT -ar 16000 $TMP
 
 # rm $TMP
-	
+
 #
 #   2.  upload audio to gc bucket (> 1:00) or local (< 1:00)
 #
@@ -76,13 +76,64 @@ gsutil cp $TMP $BUCKET
 #
 #   3.  gcloud speech recognize, return data/txt.json
 #       ** need to add case for recognize-long-running w/flag **
-#   
+#
 
 echo "gcloud recognize ..."
 
 # gcloud ml speech recognize $TMP --include-word-time-offsets --language-code='en-US' --hints=paced > $JSON
 # gcloud ml speech recognize-long-running '$BUCKET/$TMP' --include-word-time-offsets --language-code='en-US' > $JSON
-gcloud ml speech recognize-long-running 'gs://kings-speech-to-text-2/speech-16k.wav' --include-word-time-offsets --language-code='en-US' > $JSON
+#gcloud ml speech recognize-long-running 'gs://kings-speech-to-text-2/speech-16k.wav' --include-word-time-offsets --language-code='en-US' > $JSON
+
+# https://gist.github.com/cjus/1047794
+function jsonValue() {
+KEY=$1
+num=$2
+awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
+
+# https://cloud.google.com/speech-to-text/docs/reference/rest/v1p1beta1/RecognitionConfig
+NAME=$(curl -sS -H "Content-Type: application/json" \
+    -H "Authorization: Bearer "$(gcloud auth print-access-token) \
+    https://speech.googleapis.com/v1p1beta1/speech:longrunningrecognize \
+    --data '{
+  "config": {
+    "languageCode": "en-US",
+    "enableWordTimeOffsets": true,
+    "enableAutomaticPunctuation": true,
+    "useEnhanced": true,
+    "model": "video",
+    "speechContexts":{
+        "phrases": [""]
+    },
+    "metadata": {
+      "interactionType": "PRESENTATION",
+      "audioTopic": "Martin Luther King Jr."
+    }
+  },
+  "audio": {
+    "uri":"'$BUCKET'/speech-16k.wav"
+  }
+}' | jsonValue name)
+
+NAME=$(echo $NAME)
+
+echo "submit job with id "$NAME" ..."
+
+RESPONSE=""
+OUTPUT=""
+RESPONSELENGTH="$(echo $RESPONSE | wc -w | tr -d ' ')"
+while [ $RESPONSELENGTH -lt 1 ]
+do
+  OUTPUT=$(curl -sS -H "Content-Type: application/json" \
+      -H "Authorization: Bearer "$(gcloud auth print-access-token) \
+    https://speech.googleapis.com/v1/operations/$NAME)
+  RESPONSE=$(echo ${OUTPUT} | jsonValue response)
+  RESPONSELENGTH="$(echo $RESPONSE | wc -w | tr -d ' ')"
+  echo "wait ..."
+  sleep 2
+done
+echo 'received translation ...'
+echo $OUTPUT > $JSON
 
 #
 #   4.  run .pde using data/txt.json and data/speech.wav
@@ -93,5 +144,5 @@ gcloud ml speech recognize-long-running 'gs://kings-speech-to-text-2/speech-16k.
 echo "launch processing ..."
 
 pjava .
-	
+
 echo "** done **"
